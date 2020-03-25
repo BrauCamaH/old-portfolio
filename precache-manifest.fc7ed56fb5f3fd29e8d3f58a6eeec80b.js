1398ba311914cc1f6b25e310b5ac3b3f99975a62
Braulio Camarena <braulio.camarenah@gmail.com>
2020-03-25 13:34:07 -0600

    Updates

---
+++ precache-manifest.fc7ed56fb5f3fd29e8d3f58a6eeec80b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9839cebbcda0a40925b7ccc7bec9e8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c1b4f571f0fbb461e21a",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9c0ff596be34ae6384db",
+    "url": "/portfolio/static/css/main.7d6291d2.chunk.css"
+  },
+  {
+    "revision": "c1b4f571f0fbb461e21a",
+    "url": "/portfolio/static/js/2.925f999b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.925f999b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0ff596be34ae6384db",
+    "url": "/portfolio/static/js/main.ff4ddedc.chunk.js"
+  },
+  {
+    "revision": "a4251880b16282234568",
+    "url": "/portfolio/static/js/runtime-main.d7ca4f8a.js"
+  },
+  {
+    "revision": "73c3f9ff5f0f0f0219d6f110136466bf",
+    "url": "/portfolio/static/media/profile.73c3f9ff.jpg"
+  }
+]);
